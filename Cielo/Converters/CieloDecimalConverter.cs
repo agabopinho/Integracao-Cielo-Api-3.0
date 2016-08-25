87510e0f49565c2ed729d591dc8efd1b2d4c2952
Ágabo D. de Pinho <agabo.duarte@hotmail.com>
2016-08-25 10:57:33 -0300

    Namespace

--- Cielo/Converters/CieloDecimalConverter.cs
+++ Cielo/Converters/CieloDecimalConverter.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Duarti.Maverick.Cielo.Converters
+namespace Cielo.Converters
 {
     public class CieloDecimalConverter : CustomCreationConverter<decimal>
     {
